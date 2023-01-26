459c98208982629b55644653ac6d30df6de34ebd
AlexVarela <ing.alexvarela@gmail.com>
2023-01-25 18:25:43 -0600

    Implementado para 1 solo nivel y le falta una pulga

--- Laboratorio#2/src/Simon.c
+++ Laboratorio#2/src/Simon.c
@@ -1,79 +1,223 @@
-/*
-Código que utiliza interrupciones para detectar cuando se presiona un botón 
-conectado al pin y encender un LED conectado a otro pin. 
-La variable "button_press" se utiliza como una bandera para indicar si 
-se ha presionado el botón y se establece en 1 dentro de la función ISR (interrupción) 
-y se vuelve a establecer en 0 en el bucle principal del programa. 
-*/
-
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <stdlib.h> // para usar la función rand()
 
+// Volatile variables to indicate button presses
 volatile uint8_t button1_press = 0;
 volatile uint8_t button2_press = 0;
 volatile uint8_t button3_press = 0;
 volatile uint8_t button4_press = 0;
 
-ISR(INT0_vect) {
-  button1_press = 1; // Establecer la bandera de presión del botón D2
-}
+// Game variables
+volatile uint8_t game_started = 0; // nueva variable para indicar si el juego ha comenzado
+volatile uint8_t game_over = 0;    // nueva variable para indicar si el juego ha terminado
+volatile uint8_t user_sequence[4]; // nueva variable para almacenar la secuencia ingresada por el usuario
+volatile uint8_t user_errors = 0;  // nueva variable para contar los errores del usuario
 
-ISR(INT1_vect) {
-  button2_press = 1; // Establecer la bandera de presión del botón D3
-}
+// Declarar variables para el LFSR
+#define LFSR_TAP 0xCE  // Polinomio del LFSR
+#define LFSR_SEED 0x01 // Semilla del LFSR
+uint8_t lfsr = LFSR_SEED;
+uint8_t lfsr_next();
+uint8_t lsb;
 
-ISR(PCINT2_vect) {
-  button3_press = 1; // Establecer la bandera de presión del botón D4
-}
+// Declarar variables para la secuencia de LEDS
+uint8_t game_sequence[4];     // Sequence to be played
+uint8_t game_input_index = 0; // Current index of the user input
 
-ISR(PCINT1_vect) {
-  button4_press = 1; // Establecer la bandera de presión del botón A0
-}
+// void check_user_input(uint8_t button);
+//void generate_sequence();
+uint8_t game_initiated = 0; // Variable auxiliar para registrar si el juego ha sido iniciado
 
-int main(void) {
-  DDRB = 0xff; // Configurar pines B como salida (para los LED)
-  DDRD &= ~(1 << 2) & ~(1 << 3) & ~(1 << 4) & ~(1 << 5); // Configurar pines D2, D3, D4 y D5 como entrada (para los botones)
-  DDRA &= ~(1 << 0); // Configurar pin PA0 como entrada (para el botón)
 
-  // Configurar pull-up externo en los pines D2, D3, D4 y D5 (conectar una resistencia entre VCC y los pines)
-  PORTD |= (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5); // activar las resistencias pull-up
-  PORTA |= (1 << 0); // activar la resistencia pull-up
-  
-  GIMSK |= (1 << PCIE1); // Habilitar interrupciones en el pin PA0
-  GIMSK |= (1 << INT0) | (1 << INT1) | (1 << PCIE2) | (1 << PCIE1); // Habilitar interrupciones externas en los pines D2, D3, D4 y D5
-  MCUCR |= (1 << ISC01) | (1 << ISC11) | (1 << ISC10) | (1 << ISC00); // Configurar interrupciones para flanco de bajada
-  PCMSK2 |= (1 << PCINT4); // Habilitar interrupción en el pin D4
-  PCMSK1 |= (1 << PCINT8); // Habilitar interrupción en el pin PA0
-    
-sei(); // Habilitar interrupciones globales
-
-  while(1){
-    if(button1_press){
-      PORTB |= (1 << 7); // Encender LED en B7
-      _delay_ms(5000); // Esperar 500ms
-      PORTB &= ~(1 << 7); // Apagar el LED
-      button1_press = 0; // Resetear la bandera de presión del botón D2
+// Function to generate the next number in the LFSR
+uint8_t lfsr_next()
+{
+    uint8_t lsb = lfsr & 1;
+    lfsr = (lfsr >> 1) | (lsb << 7);
+    if (lsb)
+    {
+        lfsr ^= LFSR_TAP;
     }
-    if(button2_press){
-      PORTB |= (1 << 6); // Encender LED en B6
-      _delay_ms(5000); // Esperar 500ms
-      PORTB &= ~(1 << 6); // Apagar el LED
-      button2_press = 0; // Resetear la bandera de presión del botón D3
-	  }
-	  if(button3_press){
-	  PORTB |= (1 << 1); // Encender LED en B1
-    _delay_ms(5000); // Esperar 500ms
-    PORTB &= ~(1 << 1); // Apagar el LED
-    button3_press = 0; // Resetear la bandera de presión del botón D4
+    return lfsr;
+}
+
+// Function to generate the game sequence
+void generate_sequence()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        game_sequence[i] = lfsr_next() % 4;
     }
-    if(button4_press){
-    PORTB |= (1 << 0); // Encender LED en B0
-    _delay_ms(5000); // Esperar 500ms
-    PORTB &= ~(1 << 0); // Apagar el LED
-    button4_press = 0; // Resetear la bandera de presión del botón D5
+}
+
+// Function to play the game sequence
+void play_sequence()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        switch (game_sequence[i])
+        {
+        case 0:
+            PORTB |= (1 << 7); // Turn on B7
+            _delay_ms(2000);
+            PORTB &= ~(1 << 7); // Turn off B7
+            break;
+        case 1:
+            PORTB |= (1 << 6); // Turn on B6
+            _delay_ms(2000);
+            PORTB &= ~(1 << 6); // Turn off B6
+            break;
+        case 2:
+            PORTB |= (1 << 1); // Turn on B1
+            _delay_ms(2000);
+            PORTB &= ~(1 << 1); // Turn off B1
+            break;
+        case 3:
+            PORTB |= (1 << 0); // Turn on B0
+            _delay_ms(2000);
+            PORTB &= ~(1 << 0); // Turn off B0
+            break;
+        }
+        _delay_ms(2000);
+    }
+}
+
+void check_user_input(uint8_t button)
+{
+    if (game_sequence[game_input_index] == button)
+    {
+        game_input_index++;
+        if (game_input_index == 4)
+        {
+          // Completo con exito
+		game_started = 1; 
+		game_initiated = 0;  
+		// Ir a Segundo estado
+        }
+    }
+    else
+    {   
+        //perdio 
+        game_over = 1;
+	   // Ir a Tercer estado
+    }
+}
+
+
+// Interrupciones externas de los botones
+ISR(INT0_vect)
+{
+    button1_press = 1; // Establecer la bandera de presión del botón D2
+    PORTB |= (1 << 7);
+   //check_user_input(button1_press);
+    _delay_ms(2000);
+    PORTB &= ~(1 << 7); //_delay_ms(1000);
+    if (game_started == 1){
+    check_user_input(0); //button1_press);
     }	
-  }
-return 0;
+}
+
+ISR(INT1_vect)
+{
+    button2_press = 1; // Establecer la bandera de presión del botón D3
+    PORTB |= (1 << 6);
+    //check_user_input(button2_press);
+    _delay_ms(2000);
+    PORTB &= ~(1 << 6); //_delay_ms(1000);
+
+    if (game_started == 1){
+    check_user_input(1);//button2_press);
+    }	
+}
+
+ISR(PCINT2_vect)
+{
+    button3_press = 1; // Establecer la bandera de presión del botón D4
+    PORTB |= (1 << 1);
+    //check_user_input(button3_press);
+    _delay_ms(2000);
+    PORTB &= ~(1 << 1); //_delay_ms(1000);
+    if (game_started == 1){
+    check_user_input(2);//button3_press);
+    }	
+}
+
+ISR(PCINT1_vect)
+{
+    button4_press = 1; // Establecer la bandera de presión del botón A0
+    PORTB |= (1 << 0);
+    //check_user_input(button4_press);
+    _delay_ms(2000);
+    PORTB &= ~(1 << 0); //_delay_ms(1000);
+    if (game_started == 1){
+    check_user_input(3);//button4_press);
+    }	
+}
+
+
+int main()
+{
+    DDRB = 0xff;                                                        // Configurar pines B como salida (para los LED)
+    DDRD &= ~(1 << 2) & ~(1 << 3) & ~(1 << 4) & ~(1 << 5);              // Configurar pines D2, D3, D4 y D5 como entrada (para los botones)
+    DDRA &= ~(1 << 0);                                                  // Configurar pin PA0 como entrada (para el botón)
+    PORTD |= (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5);                 // activar las resistencias pull-up
+    PORTA |= (1 << 0);                                                  // activar la resistencia pull-up
+    GIMSK |= (1 << PCIE1);                                              // Habilitar interrupciones en el pin PA0
+    GIMSK |= (1 << INT0) | (1 << INT1) | (1 << PCIE2) | (1 << PCIE1);   // Habilitar interrupciones externas en los pines D2, D3, D4 y D5
+    MCUCR |= (1 << ISC01) | (1 << ISC11) | (1 << ISC10) | (1 << ISC00); // Configurar interrupciones para flanco de bajada
+    PCMSK2 |= (1 << PCINT4);                                            // Habilitar interrupción en el pin D4
+    PCMSK1 |= (1 << PCINT8);                                            // Habilitar interrupción en el pin PA0
+    sei();
+
+    // Main loop
+    while (1)
+    {
+        // Primer estado (Inicio)
+        if (game_started == 0)
+        {
+            if (button1_press || button2_press || button3_press || button4_press)
+            {
+                for (int i = 0; i < 2; i++)
+                { // parpadear los LEDS 2 veces
+                    PORTB = 0xc3;
+                    _delay_ms(5000); // esperar 500ms
+                    PORTB = 0x00;    // apagar todos los LEDS
+                    _delay_ms(5000); // esperar 500ms
+                }
+                button1_press = 0; // resetear la bandera de presión del botón D2
+                button2_press = 0; // resetear la bandera de presión del botón D3
+                button3_press = 0; // resetear la bandera de presión del botón D4
+                button4_press = 0; // resetear la bandera de presión del botón A0
+                game_started = 1;  // resetear la bandera de fin del juego
+			game_input_index = 0;
+            }
+        }
+
+        // Segundo estado (arranque de funciones)
+        if (game_started == 1 && game_initiated == 0)
+        {
+            generate_sequence();
+            play_sequence();
+            game_initiated = 1; // Actualizar la variable auxiliar para indicar que el juego ha sido iniciado
+            //game_started  = 0;
+		 //game_over = 1;
+        }
+        // tercer estado (Fin del juego)
+        if (game_over == 1)
+        { // si el usuario se equivocó
+            for (int i = 0; i < 3; i++)
+            { // parpadear los LEDS 3 veces
+                PORTB = 0x03;
+                _delay_ms(5000); // esperar 500ms
+                PORTB = 0x00;    // apagar todos los LEDS
+                _delay_ms(5000); // esperar 500ms
+            }
+            game_over = 0;    // resetear la bandera de fin del juego	
+            game_started = 0; // resetear la bandera de inicio del juego
+		  //game_initiated = 0;
+        }
+    }
+    return 0;
 }
